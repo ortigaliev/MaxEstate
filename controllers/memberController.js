be1aecac9fcbc284b9bd23b03eb18588ed586282
ortigaliev <ortigaliyevm@gmail.com>
2024-02-04 09:24:42 +0900

    feat: Develop member login process

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -5,9 +5,9 @@ let memberController = module.exports;
 memberController.signup = async (req, res) => {
   try{
     console.log("POST: cont/signup");
-    const data = req.body;
-    const member = new Member();
-    const new_member = await member.signupData(data);
+    const data = req.body,
+      member = new Member(),
+      new_member = await member.signupData(data);
 
     res.json({ state: "success", data: new_member});
   }catch(err) {
@@ -16,8 +16,18 @@ memberController.signup = async (req, res) => {
   }
 };
 
-memberController.login = (req, res) => {
-  res.send("This is login page");
+memberController.login = async (req, res) => {
+  try{
+    console.log("POST: cont/login");
+    const data = req.body,
+      member = new Member(),
+      result = await member.loginData(data);
+
+    res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/login, ${err.message}`);
+    res.json({ state: "fail", message: err.message});
+  }
 };
 memberController.logout = (req, res) => {
   res.send("This is logout page");
