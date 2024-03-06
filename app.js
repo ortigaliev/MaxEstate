32abe6161ae02e8a2e2c8773f17709ab70394fd4
ortigaliev <ortigaliyevm@gmail.com>
2024-03-06 09:36:54 +0900

    fix: Modify /prop to /agency

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const router = require("./router");
 const router_bssr = require("./router_bssr");
 
 let session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);//mongodb ni storege hosil qilishga yordam beradi
+const MongoDBStore = require("connect-mongodb-session")(session); //mongodb ni storege hosil qilishga yordam beradi
 const store = new MongoDBStore({
   uri: process.env.MONGO_URL,
   collection: "sessions",
@@ -15,8 +15,7 @@ const store = new MongoDBStore({
 //1-Entry Code
 app.use(express.static("public")); //open Public folder for requested users
 app.use(express.json()); //Converst from json format to Object
-app.use(express.urlencoded({ extended: true})); //to access requested post from HTML form
-
+app.use(express.urlencoded({ extended: true })); //to access requested post from HTML form
 
 //2-Session Code
 app.use(
@@ -31,17 +30,17 @@ app.use(
   })
 );
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.locals.member = req.session.member;
   next();
 });
 
 //3-Views Code
-app.set("views", "views");//views folder path that we created
+app.set("views", "views"); //views folder path that we created
 app.set("view engine", "ejs");
 
 //4-Router related Code
-app.use("/prop", router_bssr);//
-app.use("/", router);//This router availabes only for Admin and Agency admin users
+app.use("/agency", router_bssr); //
+app.use("/", router); //This router availabes only for Admin and Agency admin users
 
-module.exports =app;
+module.exports = app;
--- controllers/agencyController.js
+++ controllers/agencyController.js
@@ -6,41 +6,41 @@ const Definer = require("../lib/mistake");
 let agencyController = module.exports;
 
 agencyController.home = (req, res) => {
-  try{
+  try {
     console.log("GET: cont/home");
     res.render("home-page");
   } catch (err) {
     console.log(`ERROR, cont/home, ${err.message}`);
-    res.json({state: "fail", message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
-agencyController.getMyAgencyEstate = async (req, res) =>{
-  try{
+agencyController.getMyAgencyEstate = async (req, res) => {
+  try {
     console.log("GET: cont/getMyAgencyEstate");
     const estate = new Estate();
     const data = await estate.getAllEstateDataAgency(res.locals.member);
-    res.render("agency-list", {agency_data: data});
+    res.render("agency-list", { agency_data: data });
   } catch (err) {
     console.log(`ERROR, cont/getMyAgencyEstate, ${err.message}`);
     res.redirect("/prop");
   }
-}
+};
 
-agencyController.getSignupMyAgency = async (req, res) =>{
-  try{
+agencyController.getSignupMyAgency = async (req, res) => {
+  try {
     console.log("GET: cont/getSignupMyAgency");
     res.render("signup");
   } catch (err) {
     console.log(`ERROR, cont/getSignupMyAgency, ${err.message}`);
-    res.json({state: "fail", message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
-}
+};
 
 agencyController.signupProcess = async (req, res) => {
-  try{
+  try {
     console.log("POST: cont/signupProcess");
-    assert.ok(req.file, Definer.general_err3);
+    assert.ok(req.file, Definer.general_err2);
 
     let new_member = req.body;
     new_member.mb_type = "AGENCY";
@@ -51,51 +51,53 @@ agencyController.signupProcess = async (req, res) => {
     assert(result, Definer.general_err1);
 
     req.session.member = result;
-    res.redirect("/prop/estate/list");
+    res.redirect("/agency/estate/list");
   } catch (err) {
     console.log(`ERROR, cont/signupProcess, ${err.message}`);
-    res.json({state: "fail", message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 //login controller
-agencyController.getLoginMyAgency = async (req, res) =>{
-  try{
+agencyController.getLoginMyAgency = async (req, res) => {
+  try {
     console.log("GET: cont/getLoginMyAgency");
     res.render("login-page");
   } catch (err) {
     console.log(`ERROR, const/getLoginMyAgency, ${err.message}`);
-    res.json({state: "fail", message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 agencyController.loginProcess = async (req, res) => {
-  try{
+  try {
     console.log("POST: cont/loginProcess");
     const data = req.body,
-    member = new Member(),
-    result = await member.loginData(data);
+      member = new Member(),
+      result = await member.loginData(data);
 
     req.session.member = result;
     req.session.save(function () {
-      result.mb_type === "ADMIN" ? res.redirect("/prop/all-agency") : res.redirect("/prop/estate/list");
+      result.mb_type === "ADMIN"
+        ? res.redirect("/agency/all-agency")
+        : res.redirect("/agency/estate/list");
     });
   } catch (err) {
     console.log(`ERROR, const/loginProcess, ${err.message}`);
-    res.json({state: "fail", message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
 //loguot controller
 agencyController.logout = (req, res) => {
-  try{
+  try {
     console.log("GET cont/logout");
     req.session.destroy(function () {
       res.redirect("/prop");
     });
-  } catch(err) {
+  } catch (err) {
     console.log(`ERROR, cont/logout, ${err.message}`);
-    res.json({ state: "fail", message: err.message});
+    res.json({ state: "fail", message: err.message });
   }
 };
 
@@ -106,17 +108,17 @@ agencyController.validateAuthAgency = (req, res, next) => {
     next();
   } else
     res.json({
-      state: "fail", message: "Only authenticated members with agency type",
+      state: "fail",
+      message: "Only authenticated members with agency type",
     });
 };
 
-
 //check-me controller
 agencyController.checkSession = (req, res) => {
-  if (req.session?.member){
+  if (req.session?.member) {
     res.json({ state: "success", data: req.session.member });
   } else {
-    res.json({ state: "fail", message: "You are not authenticated"});
+    res.json({ state: "fail", message: "You are not authenticated" });
   }
 };
 
@@ -139,25 +141,24 @@ agencyController.getAllAgency = async (req, res) => {
   try {
     console.log("GET cont/getAllAgency");
 
-    const agency =new Agency();
+    const agency = new Agency();
     const agency_data = await agency.getAllAgencyData();
     console.log("agency_data:", agency_data);
-    res.render("all-agency", {agency_data: agency_data });
-
+    res.render("all-agency", { agency_data: agency_data });
   } catch (err) {
     console.log(`ERROR, cont/getAllAgency, ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
 };
 
-agencyController.updateAgencyByAdmin = async (req, res) =>{
-  try{
+agencyController.updateAgencyByAdmin = async (req, res) => {
+  try {
     console.log("GET cont/updateAgencyByAdmin");
-    const agency =new Agency();
+    const agency = new Agency();
     const result = await agency.updateAgencyByAdminData(req.body);
     await res.json({ state: "success", data: result });
-  } catch(err) {
+  } catch (err) {
     console.log(`ERROR, cont/updateAgencyByAdmin, ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
-  };
+};
