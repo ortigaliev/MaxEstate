de8eebc0485e9e24f371f4c08cdaa19684c53b11
ortigaliev <ortigaliyevm@gmail.com>
2024-03-07 07:16:55 +0900

    feat: Develop getChosenMember and target item view increment business logic

--- models/Member.js
+++ models/Member.js
@@ -3,6 +3,7 @@ const Definer = require("../lib/mistake");
 const assert = require("assert");
 const bcrypt = require("bcrypt");
 const { shapeIntoMongooseObjectId } = require("../lib/config");
+const View = require("./View");
 
 class Member {
   constructor() {
@@ -53,6 +54,10 @@ class Member {
       id = shapeIntoMongooseObjectId(id);
 
       console.log("member::", member);
+      if (member) {
+        //condition if not seen before
+        await this.viewChosenItemByMember(member, id, "member");
+      }
 
       const result = await this.memberModel
         .aggregate([
@@ -63,6 +68,29 @@ class Member {
 
       assert.ok(result, Definer.general_err2);
       return result[0];
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async viewChosenItemByMember(member, view_ref_id, group_type) {
+    try {
+      view_ref_id = shapeIntoMongooseObjectId(view_ref_id);
+      const mb_id = shapeIntoMongooseObjectId(member._id);
+
+      const view = new View(mb_id);
+      const isValid = await view.validateChosenTarget(view_ref_id, group_type);
+      assert.ok(isValid, Definer.general_err2);
+
+      // loged user has seen target before
+      const doesExit = await view.checkViewExistence(view_ref_id);
+      console.log("doesExist:", doesExit);
+
+      if (!doesExit) {
+        const result = await view.inserMemberView(view_ref_id, group_type);
+        assert.ok(result, Definer.general_err1);
+      }
+      return true;
     } catch (err) {
       throw err;
     }
