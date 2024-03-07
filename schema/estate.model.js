58c6096806a675f18d6d8dd0985869c4517077b3
ortigaliev <ortigaliyevm@gmail.com>
2024-03-07 09:05:11 +0900

    fix: Modify estate schema model before adding Agency data

--- schema/estate.model.js
+++ schema/estate.model.js
@@ -7,91 +7,95 @@ const {
 } = require("../lib/config");
 const Schema = mongoose.Schema;
 
-estateSchema = new mongoose.Schema({
-  estate_name: {type: String, required: true},
-  estate_collection: {
-    type: String,
-    required: true,
-    enum: {
-      values: estate_collection_enums,
-      message: "{VALUE} is not among permitted enum values",
+estateSchema = new mongoose.Schema(
+  {
+    estate_name: { type: String, required: true },
+    estate_collection: {
+      type: String,
+      required: true,
+      enum: {
+        values: estate_collection_enums,
+        message: "{VALUE} is not among permitted enum values",
+      },
+    },
+    estate_status: {
+      type: String,
+      required: false,
+      default: "PAUSED",
+      enums: {
+        values: estate_status_enums,
+        message: "{VALUE} is not among permitted enum values",
+      },
+    },
+    estate_price: {
+      type: Number,
+      required: true,
+    },
+    estate_discount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    estate_left_cnt: {
+      type: Number,
+      required: true,
+    },
+    estate_type: {
+      //product size
+      type: String,
+      default: "for sale",
+      required: function () {
+        const typed_list = ["apartment", "willa", "office", "penthouse"];
+        return typed_list.includes(this.estate_collection);
+      },
+      enum: {
+        values: estate_type_enums,
+        message: "{VALUE}, is not among permitted enum values",
+      },
+    },
+    estate_rent: {
+      //product volume
+      type: String,
+      default: "월세",
+      required: function () {
+        return this.estate_collection === "rent";
+      },
+      enum: {
+        values: estate_rent_enums,
+        message: "{VALUE}, is not among permitted enum values",
+      },
+    },
+    estate_description: {
+      type: String,
+      required: true,
+    },
+    estate_images: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+    estate_likes: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    estate_views: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    agency_mb_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Member",
+      required: false,
     },
   },
-  estate_status: {
-    type: String,
-    required: false,
-    default: "PAUSED",
-    enums: {
-      values: estate_status_enums,
-      message: "{VALUE} is not among permitted enum values",
-    },
-  },
-  estate_price: {
-    type: Number,
-    required: true,
-  },
-  estate_discount: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  estate_left_cnt: {
-    type: Number,
-    required: true,
-  },
-  estate_type: {
-    type: String,
-    default: "two-room",
-    required: function() {
-      const type_list = ["apartment","willa","house","pent-house", "office"];
-      return type_list.includes(this.estate_collection)
-    },
-    enum: {
-      values: estate_type_enums,
-      message: "{VALUE}, is not among permitted enum values",
-    },
-  },
-  estate_rent: {
-    type: String,
-    default: "월세",
-    required: function () {
-    return (this.estate_collection === "rent")
-    },
-    enum: {
-      values: estate_rent_enums,
-      message: "{VALUE}, is not among permitted enum values",
-    },
-  },
-  estate_description: {
-    type: String,
-    required: true,
-  },
-  estate_images: {
-    type: Array,
-    required: false,
-    default: []
-  },
-  estate_likes: {
-    type: Number,
-    required: false,
-    default: 0
-  },
-  estate_views: {
-    type: Number,
-    required: false,
-    default: 0
-  },
-  agency_mb_id: {
-    type:Schema.Types.ObjectId,
-    ref: "Member",
-    required: false
-  },
-},
-  {timestamps: true});//createdAt, updeatedAt
+  { timestamps: true }
+); //createdAt, updeatedAt
 
 estateSchema.index(
-  {agency_mb_id: 1, estate_name: 1, estate_size: 1, estate_rent: 1},//Doesnt allow add new product with same param 2 restaurant
-  {unique: true}
+  { agency_mb_id: 1, estate_name: 1, estate_size: 1, estate_rent: 1 }, //Doesnt allow add new product with same param 2 restaurant
+  { unique: true }
 );
 
-  module.exports = mongoose.model("Estate", estateSchema);
+module.exports = mongoose.model("Estate", estateSchema);
