5cf3020581c8d14e2b2a18c6f8e828e3e56be0d9
ortigaliev <ortigaliyevm@gmail.com>
2024-06-10 08:35:00 +0900

    feat: config.js file starting script creation

---
+++ process.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "MaxEstate",
+      swd: "./",
+      script: "./server.js",
+      watch: true,
+      env_production: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
+      instances: 2,
+      exec_mode: "cluster",
+    },
+  ],
+};
---
+++ train.js
@@ -0,0 +1,2 @@
+//Primitive command line
+//pm2 star server.js 
